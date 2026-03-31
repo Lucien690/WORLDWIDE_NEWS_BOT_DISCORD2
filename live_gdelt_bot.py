779c24cb4a49874fa1b30f129d085dde6ce6e08e
Lucien690 <lucien221119@gmail.com>
2026-03-31 07:06:42 +0200

    live_gdelt_bot.py

--- live_gdelt_bot.py
+++ live_gdelt_bot.py
@@ -28,21 +28,21 @@ KEYWORDS = [
 def get_news():
     url = "https://api.gdeltproject.org/api/v2/doc/doc"
 
-params = {
-"query": "war OR oil OR gold OR crypto OR bank OR russia OR china OR iran OR israel OR usa OR europe",
-"mode": "ArtList",
-"maxrecords": 30,
-"format": "json",
-"sort": "DateDesc"
-}
+    params = {
+    "query": "war OR oil OR gold OR crypto OR bank OR russia OR china OR iran OR israel OR usa OR europe",
+    "mode": "ArtList",
+    "maxrecords": 30,
+    "format": "json",
+    "sort": "DateDesc"
+    }
 
-response = requests.get(url, params=params)
+    response = requests.get(url, params=params)
 
-if response.status_code != 200:
-print("API ERROR:", response.text)
-return []
+    if response.status_code != 200:
+    print("API ERROR:", response.text)
+    return []
 
-return response.json().get("articles", [])
+    return response.json().get("articles", [])
 
 
 def get_score(title):
